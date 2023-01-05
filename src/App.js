448ce4b9e7a6de9606228790201f356eec4dac0d
natalia-ponomarenko <virus.zabutiya@gmail.com>
2023-01-05 12:26:16 +0200

    add styles and icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
+import $ from 'jquery';
 
 const url = "https://type.fit/api/quotes";
 
@@ -19,43 +19,47 @@ function App() {
         const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
         setQuotation(randomQuote);
       });
-    console.log(quotation);
   };
 
   useEffect(() => {
     getRandomQuote();
   }, []);
 
-
   return (
     <div className="App">
       <div className="d-flex vh-100 align-items-center">
-        <div id="quote-box" className="mx-auto w-25">
+        <div id="quote-box" className="mx-auto rounded">
           <figure>
-            <blockquote class="blockquote">
+            <blockquote className="d-flex">
+              <i className="fa-solid fa-quote-left px-1"></i>
               <div id="text">{quotation.text}</div>
             </blockquote>
-            <figcaption>
+            <figcaption className="text-end">
               <cite>
-                <span id="author">{quotation.author}</span>
+                <span id="author">{quotation.author ? quotation.author : 'Unknown' }</span>
               </cite>
             </figcaption>
           </figure>
-          <button
-            id="new-quote"
-            className="btn btn-outline-info"
-            onClick={getRandomQuote}
-          >
-            New Quote
-          </button>
-          <a
-            href="https://twitter.com/intent/tweet"
-            target="_blank"
-            id="tweet-quote"
-            rel="noreferrer"
-          >
-            Tweet
-          </a>
+          <div className="d-flex justify-content-between">
+            <button
+              type="button"
+              id="new-quote"
+              className="btn text-light"
+              onClick={getRandomQuote}
+            >
+              New Quote
+            </button>
+            <a
+              href="https://twitter.com/intent/tweet"
+              target="_blank"
+              id="tweet-quote"
+              rel="noreferrer"
+              className="p-2"
+              role="button"
+            >
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -63,3 +67,18 @@ function App() {
 }
 
 export default App;
+
+  const colors = ['#a862ea', '#6383ea','#b0ea62', '#f782c2','#f9c46b', '#85951b', '#571368']
+  let i = 0
+  const button = $('#new-quote');
+
+
+  button.on("click", function() {
+    i = i < colors.length - 1 ? ++i : 0;
+    $("#new-quote").css('background-color', colors[i]);
+    $("body").css('background-color', colors[i]);
+    $("#text").css('color', colors[i]);
+    $("#author").css('color', colors[i])
+    $(".fa-solid").css('color', colors[i]);
+    $(".fa-brands").css('color', colors[i]);
+  })
